dc44b988a23057dfa2d2e06005fa0583763aa495
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-09 21:07:51 +0530

    Creating  a somple component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 function App() {
   // There must be exactly one root element in 'return'
   return (
     <div className="App">
       <h1>Hello, World!</h1>
+      <Person />
     </div>
   );
   // Alternate way: Gets compiled as shown below
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const person = () => {
+    return (<p>I'm a person</p>);
+}
+export default person;
