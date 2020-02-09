a3f81ea6fb85693772c9f54ab9009e9a1ed33427
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-09 20:58:59 +0530

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  // There must be exactly one root element in 'return'
   return (
     <div className="App">
       <h1>Hello, World!</h1>
     </div>
   );
+  // Alternate way: Gets compiled as shown below
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello, World!'));
 }
 
 export default App;
