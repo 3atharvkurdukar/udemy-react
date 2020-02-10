6a437b838e1e23ed93cf7abd2cacff5aae9117d6
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-10 12:50:37 +0530

    Using state in functions via useState() hook

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Person from './Person/Person';
 
-class App extends Component {
-  // state only works for classes, not for functions
-  state = {
+const App = () => {
+  // This is the approach for functions
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Atharv', age: 20 },
       { name: 'Ezio', age: 26 },
       { name: 'Altaïr', age: 32 }
     ]
-  };
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
 
     const newPersons = [
       { name: 'Atharv Kurdukar', age: 20 },
       { name: 'Ezio Auditore', age: 26 },
       { name: 'Altaïr Ibn-La\'Ahad', age: 32 }
     ];
-    this.setState({
+    // WARNING: 
+    // The state variable in class modifies the current state.
+    // This replaces the current state with the new state object instead.
+    setPersonsState({
       persons: newPersons
     });
   };
 
   // There must be exactly one root element in 'return'
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, World!</h1>
-        <button onClick={this.switchNameHandler}>Full Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Occupation: Assassin</Person>
-      </div>
-    );
-    // Alternate way: Gets compiled as shown below
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello, World!'));
-  }
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <button onClick={switchNameHandler}>Full Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>Occupation: Assassin</Person>
+    </div>
+  );
 }
 
 export default App;
