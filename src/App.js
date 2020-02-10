42ec16bdc001815c0faae9b57d406a67d7624a5a
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-10 13:09:25 +0530

    Revert "Using state in functions via useState() hook" to follow class instead

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Person from './Person/Person';
 
-const App = () => {
-  // This is the approach for functions
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  // state only works for classes, not for functions
+  state = {
     persons: [
       { name: 'Atharv', age: 20 },
       { name: 'Ezio', age: 26 },
       { name: 'Altaïr', age: 32 }
     ]
-  });
+  };
 
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
 
     const newPersons = [
       { name: 'Atharv Kurdukar', age: 20 },
       { name: 'Ezio Auditore', age: 26 },
       { name: 'Altaïr Ibn-La\'Ahad', age: 32 }
     ];
-    // WARNING: 
-    // The state variable in class modifies the current state.
-    // This replaces the current state with the new state object instead.
-    setPersonsState({
+    this.setState({
       persons: newPersons
     });
   };
 
   // There must be exactly one root element in 'return'
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <button onClick={switchNameHandler}>Full Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>Occupation: Assassin</Person>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, World!</h1>
+        <button onClick={this.switchNameHandler}>Full Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Occupation: Assassin</Person>
+      </div>
+    );
+    // Alternate way: Gets compiled as shown below
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello, World!'));
+  }
 }
 
 export default App;
