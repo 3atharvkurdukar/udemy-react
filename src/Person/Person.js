8ab8627f319af6ef8d68d6f9dea50584f53bbeea
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-12 00:19:40 +0530

    Importing style using stylesheet

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 // 'props' includes the tag attributes and inner content as 'children' property
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input onChange={props.change} value={props.name} />
