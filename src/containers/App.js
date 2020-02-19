a8b1eb5f748db7fd8ba09e5c818833e79b94c049
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-19 21:02:31 +0530

    Creating HOC WithClass to apply classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 
@@ -83,7 +84,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => this.setState({ showCockpit: false })}>Remove Cockpit</button>
         {this.state.showCockpit ? 
           <Cockpit 
@@ -92,7 +93,7 @@ class App extends Component {
             personsLength={this.state.persons.length}
             clicked={this.togglePersonsHandler} /> : null }
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
