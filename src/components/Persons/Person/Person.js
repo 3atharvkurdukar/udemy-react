90046e7af57927f33471c9c729a58def82a3f800
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-19 19:30:13 +0530

    Using array to render multiple JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,13 +7,11 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
   
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>
-        <p>{this.props.children}</p>
+    return [
+        <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>,
+        <p>{this.props.children}</p>,
         <input onChange={this.props.change} value={this.props.name} />
-      </div>
-    );
+    ];
   }
 }
 export default Person;
