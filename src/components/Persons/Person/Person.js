2986e52955dc89c0c1de09f5c9bbbd70b61e9c47
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-19 19:42:57 +0530

    Using Auxiliary as Higher Order  Component to return multiple JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Person.module.css';
+import Auxiliary from  '../../../hoc/Auxiliary';
 
 class Person extends Component {
 
@@ -7,11 +8,13 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
   
-    return [
-        <p key="ncwi4t" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>,
-        <p key="877t4f">{this.props.children}</p>,
+    return (
+      <Auxiliary>
+        <p key="ncwi4t" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>
+        <p key="877t4f">{this.props.children}</p>
         <input key="hg94bn" onChange={this.props.change} value={this.props.name} />
-    ];
+      </Auxiliary>
+    );
   }
 }
 export default Person;
