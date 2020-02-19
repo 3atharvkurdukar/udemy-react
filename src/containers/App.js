63828c4e4321354020bad5b78b6db13f3313171c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-19 21:34:03 +0530

    Alternate method to implement HOC

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
 
 class App extends Component {
 
@@ -84,7 +85,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => this.setState({ showCockpit: false })}>Remove Cockpit</button>
         {this.state.showCockpit ? 
           <Cockpit 
@@ -93,9 +94,9 @@ class App extends Component {
             personsLength={this.state.persons.length}
             clicked={this.togglePersonsHandler} /> : null }
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
