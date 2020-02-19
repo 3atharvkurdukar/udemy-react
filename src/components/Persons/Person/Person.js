cb260cea7e6b2fc853850761c249000187a1c09f
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-19 19:31:50 +0530

    Using key for JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,9 +8,9 @@ class Person extends Component {
     console.log('[Person.js] rendering...');
   
     return [
-        <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>,
-        <p>{this.props.children}</p>,
-        <input onChange={this.props.change} value={this.props.name} />
+        <p key="ncwi4t" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>,
+        <p key="877t4f">{this.props.children}</p>,
+        <input key="hg94bn" onChange={this.props.change} value={this.props.name} />
     ];
   }
 }
