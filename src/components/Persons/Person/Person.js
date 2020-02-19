426f9a03369ee06f17bd99d1d480bb4f0a8ce587
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-19 20:44:28 +0530

    Using React Fragment as HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import classes from './Person.module.css';
-import Auxiliary from  '../../../hoc/Auxiliary';
 
 class Person extends Component {
 
@@ -8,12 +7,13 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
   
+    // React.Fragment works exactly like Auxiliary and is built into React
     return (
-      <Auxiliary>
+      <React.Fragment>
         <p key="ncwi4t" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old.</p>
         <p key="877t4f">{this.props.children}</p>
         <input key="hg94bn" onChange={this.props.change} value={this.props.name} />
-      </Auxiliary>
+      </React.Fragment>
     );
   }
 }
