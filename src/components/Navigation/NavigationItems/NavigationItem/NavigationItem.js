41b9cf337fda5da91804e5d97d819fa47876021f
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-23 15:35:37 +0530

    Fixing the active NavigationItem

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,7 @@ import classes from './NavigationItem.module.css';
 const navigationItem = (props) => {
     return (
         <li className={classes.NavigationItem}>
-            <a href={props.link} className={props.active ? 'classes.active' : null }>
+            <a href={props.link} className={props.active ? classes.active : null }>
                 {props.children}
             </a>
         </li>
