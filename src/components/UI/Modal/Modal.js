c08dad43899b08777e8000eadf08f8ac17a13556
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-28 20:59:04 +0530

    Improving  code by removing unnecessary updation of Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary'
 import Backdrop from '../Backdrop/Backdrop';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed} />
-            <div className={classes.Modal} style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-                { props.children }
-            </div>
-        </Aux>
-    );
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (nextProps.show !== this.props.show);
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal] componentDidUpdate');
+    }
+    
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal} style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                    { this.props.children }
+                </div>
+            </Aux>
+        );
+    }
 }
 
-modal.propTypes = {
-    show: propTypes.bool,
-    modalClosed: propTypes.func
+Modal.propTypes = {
+    show: PropTypes.bool,
+    modalClosed: PropTypes.func
 };
 
-export default modal;
+export default Modal;
