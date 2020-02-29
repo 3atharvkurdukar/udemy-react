8e290938c713bbddc561f268936dc50a02d8743c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-03-01 01:30:18 +0530

    Creating and using axios instances

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
+import axios from '../../my-axios';
 
 class Blog extends Component {
 
