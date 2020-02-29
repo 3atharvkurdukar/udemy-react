0948d56ca17b11375609de08cd7278e6ac1e10c9
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-02-29 15:35:25 +0530

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
 }
 
 export default App;
