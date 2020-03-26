07e5d9ca3b3da96f157e39453c079ec6e3097b67
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-03-26 17:39:53 +0530

    Added routes to home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 
+import Courses from './containers/Courses/Courses';
+import Users from './containers/Users/Users';
+import { BrowserRouter, NavLink, Switch, Route, Redirect } from 'react-router-dom';
+
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <ol style={{textAlign: 'left'}}>
-          <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-          <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-          <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-          <li>Pass the course ID to the "Course" page and output it there</li>
-          <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-          <li>Load the "Course" component as a nested component of "Courses"</li>
-          <li>Add a 404 error page and render it for any unknown routes</li>
-          <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-        </ol>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header>
+            <NavLink to="/courses"><button>Courses</button></NavLink>
+            &nbsp;
+            <NavLink to="/users"><button>Users</button></NavLink>
+          </header>
+          <Switch>
+            <Route path="/courses" component={Courses} />
+            <Route path="/users" component={Users} />
+            <Redirect from="/all-courses" to="/courses" />
+            <Route exact path="/" component={Courses} />
+            <Route render={() => (
+              <h2 style={{ textAlign: 'center' }}>
+                Page Not Found
+              </h2>
+            )} />
+          </Switch>
+          <ol style={{textAlign: 'left'}}>
+            <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
+            <li>Pass the course ID to the "Course" page and output it there</li>
+            <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
+            <li>Load the "Course" component as a nested component of "Courses"</li>
+          </ol>
+        </div>
+      </BrowserRouter>
     );
   }
 }
