7d054d10a37c378629363deeecc2e44c2e97a335
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-03-26 17:42:57 +0530

    Searching query params and outputting course data

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+
+    state = {
+        id: null,
+        title: ''
+    }
+
+    loadData() {
+        const query = new URLSearchParams(this.props.location.search);
+        if (this.state.id !== query.get('id')) {
+            this.setState({
+                id: query.get('id'),
+                title: query.get('title')
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
     render () {
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.state.title}</h1>
+                <p>Course ID: {this.state.id}</p>
             </div>
         );
     }
