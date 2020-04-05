53b7cd592cfde784fd95913f825a00993a13722f
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-05 15:04:02 +0530

    Navigating to checkout page & passing query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,24 +1,52 @@
-import React, { Component } from 'react'
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import React, { Component } from "react";
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1,
+    },
+  };
 
-    state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
     }
+    this.setState({
+      ingredients: ingredients,
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
-            </div>
-        );
-    }
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
+
+  render() {
+    return (
+      <div>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          onCheckoutCancelled={this.checkoutCancelledHandler}
+          onCheckoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
+        />
+      </div>
+    );
+  }
 }
 
 export default Checkout;
