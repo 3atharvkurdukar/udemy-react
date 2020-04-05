bd8a26522ca1d23036a5e4a6e277d8ac2ec47d52
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-05 13:22:36 +0530

    Add react router to the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Route exact path="/" component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
       </div>
     );
