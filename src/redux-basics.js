17baeec2adbb16051c68dbe4f6f1b4904811b029
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-07 23:57:49 +0530

    Dispatching actions

--- src/redux-basics.js
+++ src/redux-basics.js
@@ -7,7 +7,20 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "INC_COUNTER":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "ADD_COUNTER":
+      return {
+        ...state,
+        counter: state.counter + action.value,
+      };
+    default:
+      return state;
+  }
 };
 
 // Store
@@ -15,5 +28,13 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({
+  type: "INC_COUNTER",
+});
+store.dispatch({
+  type: "ADD_COUNTER",
+  value: 10,
+});
+console.log(store.getState());
 
 // Subscription
