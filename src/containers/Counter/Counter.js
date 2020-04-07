df49b20d8cdfe41584687baab39cf4ef8482d2b8
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-08 01:04:27 +0530

    Dispatching actions from within component

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -40,19 +40,19 @@ class Counter extends Component {
         <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="Increment"
-          clicked={() => this.counterChangedHandler("inc")}
+          clicked={this.props.onIncrementCounter}
         />
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
+          clicked={this.props.onDecrementCounter}
         />
         <CounterControl
           label="Add 5"
-          clicked={() => this.counterChangedHandler("add", 5)}
+          clicked={() => this.props.onAddCounter(5)}
         />
         <CounterControl
           label="Subtract 5"
-          clicked={() => this.counterChangedHandler("sub", 5)}
+          clicked={() => this.props.onSubtractCounter(5)}
         />
       </div>
     );
@@ -65,4 +65,13 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: (value) => dispatch({ type: "ADD", value: value }),
+    onSubtractCounter: (value) => dispatch({ type: "SUBTRACT", value }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,25 @@ const initialState = {
   counter: 0,
 };
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.value,
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.value,
+      };
+  }
   return state;
 };
 export default reducer;
