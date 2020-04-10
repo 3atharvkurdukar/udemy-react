b17a69cb0b68fd6d990dfc1b7fb11c4a03266d00
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-10 22:02:59 +0530

    Installing redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore } from "redux";
 import reducer from "./store/reducer";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
