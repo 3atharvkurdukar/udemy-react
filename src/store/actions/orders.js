bd1c2e7018d759e6bfabfd99ed8a8282bdfc787c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-10 23:50:24 +0530

    Adding order actions

---
+++ src/store/actions/orders.js
@@ -0,0 +1,30 @@
+import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
+
+export const purchaseBurgerSuccess = (orderId, orderData) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId,
+    orderData,
+  };
+};
+
+export const purchaseBurgerFail = (error) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_ERROR,
+    error,
+  };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+  return (dispatch) => {
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        dispatch(purchaseBurgerSuccess(response.data, orderData));
+      })
+      .catch((error) => {
+        dispatch(purchaseBurgerFail(error));
+      });
+  };
+};
