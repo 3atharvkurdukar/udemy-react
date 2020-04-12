2fcfc7ba43f1ac031139b678403ba7f238da8202
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-12 14:51:19 +0530

    Storing the token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -10,7 +10,8 @@ export const authStart = () => {
 export const authSuccess = (authData) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData,
+    token: authData.idToken,
+    userId: authData.localId,
   };
 };
 
