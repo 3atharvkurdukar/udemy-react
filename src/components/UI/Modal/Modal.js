1e52414fe4fe6ec036a217d3a6e2728440840f8c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-23 00:14:43 +0530

    Removing console.log statements

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
-import classes from './Modal.module.css';
-import Aux from '../../../hoc/Auxiliary'
-import Backdrop from '../Backdrop/Backdrop';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import classes from "./Modal.module.css";
+import Aux from "../../../hoc/Auxiliary";
+import Backdrop from "../Backdrop/Backdrop";
+import PropTypes from "prop-types";
 
 class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextProps.show !== this.props.show ||
+      nextProps.children !== this.props.children
+    );
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return (nextProps.show !== this.props.show || nextProps.children !== this.props.children);
-    }
-
-    componentDidUpdate() {
-        console.log('[Modal] componentDidUpdate');
-    }
-    
-    render() {
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div className={classes.Modal} style={{
-                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: this.props.show ? '1' : '0'
-                }}>
-                    { this.props.children }
-                </div>
-            </Aux>
-        );
-    }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
 }
 
 Modal.propTypes = {
-    show: PropTypes.bool,
-    modalClosed: PropTypes.func
+  show: PropTypes.bool,
+  modalClosed: PropTypes.func,
 };
 
 export default Modal;
