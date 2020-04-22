0230b6845280de4797dc9131dff55b54fd7d6d1d
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-22 23:45:24 +0530

    Using checkValidity as a utility function

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,7 +6,7 @@ import * as actions from "../../store/actions/index";
 import { connect } from "react-redux";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import { Redirect } from "react-router";
-import { updateObject } from "../../utility/utility";
+import { updateObject, checkValidity } from "../../utility/utility";
 
 class Auth extends Component {
   state = {
@@ -49,33 +49,11 @@ class Auth extends Component {
     }
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-    if (rules.minlength) {
-      isValid = value.trim().length >= rules.minlength && isValid;
-    }
-    if (rules.maxlength) {
-      isValid = value.trim().length <= rules.maxlength && isValid;
-    }
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value.trim().toLowerCase()) && isValid;
-    }
-
-    return isValid;
-  }
-
   inputChangedHandler = (event, controlName) => {
     const updatedControls = updateObject(this.state.controls, {
       [controlName]: updateObject(this.state.controls[controlName], {
         value: event.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           event.target.value,
           this.state.controls[controlName].validation
         ),
--- src/utility/utility.js
+++ src/utility/utility.js
@@ -4,3 +4,25 @@ export const updateObject = (oldObject, updatedProperties) => {
     ...updatedProperties,
   };
 };
+
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+  if (!rules) {
+    return true;
+  }
+  if (rules.required) {
+    isValid = value.trim() !== "" && isValid;
+  }
+  if (rules.minlength) {
+    isValid = value.trim().length >= rules.minlength && isValid;
+  }
+  if (rules.maxlength) {
+    isValid = value.trim().length <= rules.maxlength && isValid;
+  }
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid = pattern.test(value.trim().toLowerCase()) && isValid;
+  }
+
+  return isValid;
+};
