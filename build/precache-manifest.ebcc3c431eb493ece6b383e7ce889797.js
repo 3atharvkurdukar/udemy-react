389c4cc3f03a41f7f73480a25ee18b5153c70dca
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-04-24 15:26:22 +0530

    Run `npm build` command

---
+++ build/precache-manifest.ebcc3c431eb493ece6b383e7ce889797.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d56b1a00600c3e13acb8f1fc20be561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df5fe3de785ed30603db",
+    "url": "/static/css/3.9398b9ef.chunk.css"
+  },
+  {
+    "revision": "81346ed2a89b11cea76f",
+    "url": "/static/css/4.22c06e87.chunk.css"
+  },
+  {
+    "revision": "ab6ab834f560a9e3256f",
+    "url": "/static/css/main.7540cbdd.chunk.css"
+  },
+  {
+    "revision": "b1fd1d32a9b94c352b96",
+    "url": "/static/js/2.69ece825.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.69ece825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5fe3de785ed30603db",
+    "url": "/static/js/3.a931d05f.chunk.js"
+  },
+  {
+    "revision": "81346ed2a89b11cea76f",
+    "url": "/static/js/4.0cc56bf7.chunk.js"
+  },
+  {
+    "revision": "ab6ab834f560a9e3256f",
+    "url": "/static/js/main.d1475b55.chunk.js"
+  },
+  {
+    "revision": "bcb2468a12cdfa3c90f2",
+    "url": "/static/js/runtime-main.521d4772.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
