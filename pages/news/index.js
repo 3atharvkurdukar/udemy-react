af229d5ee45fa89c4b05357fa8efc6b7bde8cb2c
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-06-29 00:58:45 +0530

    Navigation between pages using Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+
 export default function News() {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-great-framework">
+            Next JS is a great framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
